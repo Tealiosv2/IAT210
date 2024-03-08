bce006d212167c67f2d719deb629162095e20f5a
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2024-03-07 17:13:37 -0800

    Update iat.js

--- iat.js
+++ iat.js
@@ -1,15 +1,35 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// Creating an Express application
-const app = express();
-const port = 10000; // You can use any port you prefer
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // If the URL is '/', serve the iat.html file
+    const filePath = path.join(__dirname, './index.html');
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    // Read the content of the HTML file
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) {
+        // If there is an error reading the file, return a 500 response
+        res.writeHead(500);
+        res.end('Internal Server Error');
+      } else {
+        // Set the appropriate content type for HTML
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        
+        // Send the HTML content as the response
+        res.end(content);
+      }
+    });
+  } else {
+    // If the URL is not '/', return a 404 response
+    res.writeHead(404);
+    res.end('Not Found');
+  }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const port = 10000; // You can change this to any available port you prefer
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
